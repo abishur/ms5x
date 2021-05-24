ef6901974b8654a1d4bf3fb49d3f7eae30334a73
abishur <abishurp@gmail.com>
2021-05-24 10:49:05 -0500

    Update MS5x.h
    Cleaned up/organized code

--- src/MS5x.h
+++ src/MS5x.h
@@ -21,7 +21,7 @@
 #ifndef MS5x_h
 #define MS5x_h
 
-#define MS5611 // Comment out this line if you are using the MS5607
+#define MS5611
 
 #include "Arduino.h"
 #include <Wire.h>
@@ -32,36 +32,41 @@
 
 // I2C commands of chip
 #define MS5xxx_CMD_RESET	0x1E    // perform reset
-#define MS5xxx_CMD_ADC_READ	0x00    // initiate read sequence
-#define MS5xxx_CMD_ADC_CONV 	0x40    // start conversion
-#define MS5xxx_CMD_ADC_D1   	0x00    // read ADC 1
-#define MS5xxx_CMD_ADC_D2   	0x10    // read ADC 2
-#define MS5xxx_CMD_ADC_256  	0x00    // set ADC oversampling ratio to 256
-#define MS5xxx_CMD_ADC_512  	0x02    // set ADC oversampling ratio to 512
-#define MS5xxx_CMD_ADC_1024 	0x04    // set ADC oversampling ratio to 1024
-#define MS5xxx_CMD_ADC_2048 	0x06    // set ADC oversampling ratio to 2048
-#define MS5xxx_CMD_ADC_4096 	0x08    // set ADC oversampling ratio to 4096
-#define MS5xxx_CMD_PROM_RD  	0xA0    // initiate readout of PROM registers
+#define MS5xxx_CMD_ADC_READ 0x00    // initiate read sequence
+#define MS5xxx_CMD_ADC_CONV 0x40    // start conversion
+#define MS5xxx_CMD_ADC_D1   0x00    // read ADC 1
+#define MS5xxx_CMD_ADC_D2   0x10    // read ADC 2
+#define MS5xxx_CMD_ADC_256  0x00    // set ADC oversampling ratio to 256
+#define MS5xxx_CMD_ADC_512  0x02    // set ADC oversampling ratio to 512
+#define MS5xxx_CMD_ADC_1024 0x04    // set ADC oversampling ratio to 1024
+#define MS5xxx_CMD_ADC_2048 0x06    // set ADC oversampling ratio to 2048
+#define MS5xxx_CMD_ADC_4096 0x08    // set ADC oversampling ratio to 4096
+#define MS5xxx_CMD_PROM_RD  0xA0    // initiate readout of PROM registers
 
 class MS5x
 {
   protected:
 	// Functions
-	unsigned char send_cmd(uint8_t aCMD); // Sends a conversion command or read request to sensor
-	uint32_t read_adc(); // Converts D1 and D2 into scaled values.
-	uint8_t Calc_CRC4(uint16_t nprom[]); // Calculate expected CRC4
-    	uint8_t Read_CRC4(); // Read CRC4 from sensor
 	void ReadProm(); // Reads factory scaling data, only needs to be read once at startup (or again after sensor reset) 
 	
+	uint8_t Calc_CRC4(uint16_t nprom[]); // Calculate expected CRC4
+	uint8_t Read_CRC4(); // Read CRC4 from sensor
+	uint8_t send_cmd(uint8_t aCMD); // Sends a conversion command or read request to sensor
+	
+	uint32_t read_adc(); // Converts D1 and D2 into scaled values.
+    	
 	// Variables
 	bool hasUpdates = false; // True when pressure and temperature values have updated
-	int8_t readStep = 0; // Used to handle polling for sensor
+	
 	int8_t i2caddr; // i2c address of sensor
+	
+	uint8_t readStep = 0; // Used to handle polling for sensor
 	uint8_t sampleRate = 0x08; // Oversampling rate, default is 4096.  Set using setSamples(aCMD) where aCMD is value as defined in definition area above (MS5xxx_CMD_ADC_####)
 	
 	uint16_t C[8]; // Calibration Coefficents
 	
-	uint32_t D1=0, D2=0; // D1 = unconverted pressure reading, D2 = unconverted temperature reading
+	uint32_t D1=0; // Unscaled pressure reading
+	uint32_t D2=0; // Unscaled temperature reading
 	uint32_t prevRead = 0; // Time (in millis) of previous read step.
 	uint32_t readDelay = 10; // Time in MS to wait between read requests from the device, default is minimum required delay for maximum oversampling
 	uint32_t readDelayPrev = 10; // Holds read delay value when preforming Reset.
@@ -73,21 +78,20 @@ class MS5x
 	
   public:
 	// Functions
-    	MS5x(TwoWire *aWire); // Constructor function
+	MS5x(TwoWire *aWire); // Constructor function
 	
-    	void setI2Caddr(int8_t aAddr); // Sets I2C address
-    	uint8_t connect(uint8_t aCMD = MS5xxx_CMD_ADC_4096); // Connects to device and sets oversampling ratio.  Default is max oversampling
-    
-    	bool Readout(int32_t offset=0); // Converts Temperature and Pressure readings to Celcius and Mbar, offset is used to fix errors in temperature readings where 100 = 1.00 Celcious
+	void setI2Caddr(int8_t aAddr); // Sets I2C address
+	void setSamples(uint8_t aCMD);
+		
+	uint8_t connect(uint8_t aCMD = MS5xxx_CMD_ADC_4096); // Connects to device and sets oversampling ratio.  Default is max oversampling
+	uint8_t CRCcodeTest(); // Check validity of CRC, not working currently
+
+	bool Readout(int32_t offset=0); // Converts Temperature and Pressure readings to Celcius and Mbar, offset is used to fix errors in temperature readings where 100 = 1.00 Celcious
+	bool checkCRC();
+	bool checkUpdates();
 	
-	bool checkCRC();  // Check validity of CRC
-    	uint8_t CRCcodeTest(); // Used to make sure the Calc_CRC4 function was working correctly serves no function in actual program beyond showing an example of how CRC check works
-	
-	void setSamples(uint8_t aCMD); // Set the oversampling ratio, use one of the MS5xxx_CMD_ADC_#### defined above
-	bool checkUpdates(); // Include in main loop, will perform temp/pressure polling and return true when updated values are available
-    
-    	double GetTemp(); // Get Temperature
-    	double GetPres(); // Get Pressure
+	double GetTemp();
+	double GetPres();
 	
 	// Variables
 };
